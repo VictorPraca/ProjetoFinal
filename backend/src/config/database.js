79fc71d65da193c669109f90644422375685b8fa
Victor Praça <victorpraca@gmail.com>
2025-06-16 09:44:59 -0300

    Update database.js

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
+  port: process.env.DB_PORT || 3307,
   logging: false, 
 });
 
